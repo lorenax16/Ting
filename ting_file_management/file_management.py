013d226e16ac59e5a13e10b268a2600797ad6547
lorena olivares <lorena.olivares.perez16@gmail.com>
2023-02-06 16:20:18 -0300

    começando req 1 e 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if path_file.endswith(".txt"):
+        try:
+            with open(path_file, "r") as file:
+                document = file.read()
+                return document.split("\n")
+        except FileNotFoundError:
+            print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+    else:
+        print("Formato inválido", file=sys.stderr)
