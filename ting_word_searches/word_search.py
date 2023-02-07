276e92bfec69dcb0833755591d59f00c270c81ca
lorena olivares <lorena.olivares.perez16@gmail.com>
2023-02-07 12:37:03 -0300

    req 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,26 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    data = []
+    lower_case = word.lower()
+    lines = 0
+
+    for i in range(len(instance)):
+
+        file = instance.search(i)
+
+        saida = {
+            "palavra": lower_case,
+            "arquivo": file["nome_do_arquivo"],
+            "ocorrencias": [],
+        }
+
+    for linea in file["linhas_do_arquivo"]:
+        lines += 1
+        if lower_case in linea.lower():
+            saida["ocorrencias"].append({"linha": lines})
+
+    if saida["ocorrencias"]:
+        data.append(saida)
+    return data
 
 
 def search_by_word(word, instance):
